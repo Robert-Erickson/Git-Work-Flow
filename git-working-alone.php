98ac9555cd945f31284321b9c4c41a5bb2fe89ac
Robert Michael Erickson <robert.erickson@uvm.edu>
2018-04-10 19:33:37 -0400

    Setting up a repo so we can share our ideas for using git. The hope is to be consistent across classes.

---
+++ git-working-alone.php
@@ -0,0 +1,171 @@
+<?php
+include '../top.php';
+?>
+
+<article id="assignments" class ="lab">
+    <h1>GIT setup Working Alone</h1>
+
+    <figure class="img-left onehundred"><img src="//rerickso.w3.uvm.edu/Blackboard-live/cs142/images/github.jpg" alt=""><figcaption>Git work flow model</figcaption></figure>
+
+    <p>The work flow process that we will be using for this class is to work on your computer and sftp (run your NetBeans project) your files to cs142/<em>dev-folderName</em> folder to make sure it works. When it works you then go to your computer using git and commit your changes and push to gitlab. Then you will ssh to the server and go to your live folder where you  pull down from git lab. It is very important that you never ftp (sftp) to the live folders. So lets get this all set up. In the end your live sites will now be ready (right now it is just your sitemap and lab 1.</p>
+
+<div style="text-align: center; border-radius: 0.2em; margin-left: 2em; margin-right: 1em; padding: 0.5em;">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLxWIQk1hqg0_8M-snr4d9dltV_-8GoDgt" frameborder="0" allowfullscreen></iframe>
+    <div style="color: #839e99; font-style: italic; text-align: center;">Demonstration of the git work flow process</div>
+</div>
+
+
+    <p> Git has a great <a href="https://try.github.io/levels/1/challenges/2" target="_blank">tutorial</a> to help the beginner.</p>
+
+    <form id="labForm" name="labForm">
+
+        <label><input type="checkbox"> Before starting this lab be sure to have <a href="https://git-scm.com/downloads" target="_blank">git installed</a> on your computer.</label>
+        
+<label><input type="checkbox"> Create a sitemap for this class.</label>
+
+        <label><input type="checkbox"> Create a <a href="https://gitlab.uvm.edu" target="_blank">gitlab account</a> at uvm (just sign in with your uvm account).</label>
+
+        <label><input type="checkbox"> Create a new GitLab project named CS-142-lab1.</label>
+
+        <label><input type="checkbox"> Keep the content of the first Gitlab page (has all the git commands) in a text file in your lab folder named gitcommands.txt. Make sure it is SSH as shown below (if it shows https, then click the drop down and choose ssh, then copy the commands):
+        <figure>
+                <img src="//rerickso.w3.uvm.edu/Blackboard-live/cs142/images/git-lab-ssh.png">
+                <figcaption>SSH vs HTTPS</figcaption></figure></label>
+
+        <label><input type="checkbox"> On your computer set up this folder structure (use cs142):
+            <figure>
+                <img src="//rerickso.w3.uvm.edu/Blackboard-live/cs142/images/cs142-folder-structure.png">
+                <figcaption>cs 142 folder structure. NOTE: you will NEVER put anything in the live folders, only the dev folders.</figcaption>
+            </figure>
+        </label>
+
+        <label><input type="checkbox"> Set up your cs142/<span class="highlight">dev-lab1</span> folder to be a git repo (git init) on your computer.
+            <div style="text-align: center; border-radius: 0.2em; margin-left: 2em; margin-right: 1em; padding: 0.5em;">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/fW8Tp4ni81Q" frameborder="0" allowfullscreen></iframe>
+                <div style="color: #839e99; font-style: italic; text-align: center;">Setting up your computer to be a git repo.</div>
+            </div>
+
+        </label>
+        <label><input type="checkbox"> Now connect your local computer to your git repo: 
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">git</span> remote add origin git@gitlab.uvm.edu:YOURNAME/YOUR-REPO-NAME.git
+                </pre></div>
+        </label>
+        <label><input type="checkbox"> Add a link to your git repo: https://gitlab.uvm.edu/your-name  on your sitemap (this helps you because we cannot see your repo since it is private :).</label>
+
+        <label><input type="checkbox"> In your dev folder: Create a file named: .user.ini
+
+            <div style="text-align: center; border-radius: 0.2em; margin-left: 2em; margin-right: 1em; padding: 0.5em;">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/NIZ0Q0aUO2w" frameborder="0" allowfullscreen></iframe>
+                <div style="color: #839e99; font-style: italic; text-align: center;">Setting up a .user.ini file to display errors</div>
+            </div>
+
+        </label>
+        <label><input type="checkbox"> .user.ini has one line in it:
+            <div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">display_errors</span> = <span style="color: #009999">1</span>;
+                </pre></div>
+
+        </label>
+
+        <label><input type="checkbox"> In your dev folder: Create a file named .gitignore
+            <div style="text-align: center; border-radius: 0.2em; margin-left: 2em; margin-right: 1em; padding: 0.5em;">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/0Fd1-EuZv8g" frameborder="0" allowfullscreen></iframe>
+                <div style="color: #839e99; font-style: italic; text-align: center;">Creating a .gitignore file</div>
+            </div></label>
+
+        <label><input type="checkbox"> .gitignore should contain this:
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%">.htaccess
+.user.ini
+ADMIN
+nbproject
+                </pre></div>
+
+        </label>
+
+        <label><input type="checkbox"> Copy your cs 008 final project into the dev-lab1 folder. NOTE: Do not have cs142/dev-lab1/cs008/lab1/finalproject/index.php but instead have cs142/dev-lab1/index.php</label>
+
+
+        <label><input type="checkbox"> Run your Netbeans project to sftp (run your NetBeans project) the all files.</label>
+        
+        
+        <label><input type="checkbox"> Check to make sure your final project is working (dev), there should be no change that you can see. Edit any files as needed (ie you may need to edit top.php to point to the CSS file).</label>
+        <label><input type="checkbox"> Set up the ssh key on your computer (this step you only need to do once on your computer, the other steps you do for every repo/project)
+            <div style="text-align: center; border-radius: 0.2em; margin-left: 2em; margin-right: 1em; padding: 0.5em;">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/NMNUnI_oIMo" frameborder="0" allowfullscreen></iframe>
+                <div style="color: #839e99; font-style: italic; text-align: center;">Setting up ssh key on your computer.</div>
+            </div>
+        </label>
+
+
+
+        <label><input type="checkbox"> Since you are happy with the code you have, add this code to your version
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">git</span> add -A
+                </pre></div>
+        </label>
+
+        <label><input type="checkbox"> When you make a commit you are telling git this version is good:
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">git</span> commit -m <span style="color: #aa5500">&quot;message about what you did&quot;</span>
+                </pre></div>
+
+            NOTE: If you forget the -m with a message you will go into the vim editor. Press these keys in this order ESC : qw to get out and save</label>
+
+        <label><input type="checkbox"> You send your version to your cloud repo (ie gitlab or github)
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">git</span> push origin master</pre></div>
+        </label>
+
+        <h2>Set up the live site.</h2>
+
+        <label><input type="checkbox"> Connect to the server and get into your cs 142 folder (not it is minus lower case L):
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">ssh</span> w3.uvm.edu -l yourusername [hit enter, type your password]
+
+<span style="color: #00aaaa">cd</span> www-root/cs142/ [hit enter]
+                </pre></div>
+        </label>
+
+     <label><input type="checkbox"> Make the live folder:
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%">
+        <span style="color: #00aaaa">mkdir</span> live-lab1 [hit enter]
+                </pre></div>
+        </label>
+        <label><input type="checkbox"> Go into the live folder:
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%">
+        <span style="color: #00aaaa">cd</span> live-lab1 [hit enter]
+                </pre></div>
+        </label>
+        <label><input type="checkbox"> Set up the ssh key on the server (this step you only need to do once on the server, the other steps you do for every repo/project)
+            <div style="text-align: center; border-radius: 0.2em; margin-left: 2em; margin-right: 1em; padding: 0.5em;">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/8AbSq8wVAwc" frameborder="0" allowfullscreen></iframe>
+                <div style="color: #839e99; font-style: italic; text-align: center;">Setting up ssh key on your web server.</div>
+            </div>
+        </label>
+        <label><input type="checkbox"> Check your status, if it is not a repo you will need to initialize it:<!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">git</span> status
+
+<span style="color: #00aaaa">git</span> init
+                </pre></div>
+        </label>
+
+        <label><input type="checkbox"> Connect your live folder with the repo (same as connecting your local machine to the repo)
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">git</span> remote add origin git@gitlab.uvm.edu:YOURNAME/YOUR-REPO-NAME.git
+                </pre></div>
+        </label>
+
+        <label><input type="checkbox"> The last step is pull your files down from the repo to your live site.
+            <!-- HTML generated using hilite.me --><div style="text-indent:0; background: #ffffff; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em 2.6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #00aaaa">git</span> pull origin master
+                </pre></div>
+        </label>
+
+        <label><input type="checkbox"> Check the live site, it should work the same as the dev site except errors and warning do not show.</label>
+
+        <h2>What to submit in blackboard:</h2>
+        <p>Since these files are the same as the repo we don't need them all, Just the ones listed below:</p>
+<label><input type="checkbox"> View your gitlab repo and print to a pdf so we can see it (named repo.pdf)</label>
+        <label><input type="checkbox"> Submit a screen shot of .gitignore named gitignore.png (or .jpg)</label>
+        <label><input type="checkbox"> Submit a screen shot of .user.ini named userini.png (or .jpg)</label>
+        <label><input type="checkbox"> Add a comment with the URL for your live site ex:<br>http://YOUR-USER-NAME.w3.uvm.edu/cs142/live-lab1 (should of course also be on your sitemap).</label>
+        <label><input type="checkbox"> Remember your site map should have a link to each file, including all the images above. Normally we don't include images but these images are for grading purposes.</label>
+    </form>
+    
+    <h2>NOTE:</h2>
+    <p>This is how you will set up every project EXCEPT you will not have to do the keys as they are already set up</p>
+</article>
+</body>
+</html>
---
+++ git-working-group.php
@@ -0,0 +1,128 @@
+<?php
+include '../top.php';
+?>
+
+<article id="assign" class ="lab">
+    <h1>Git to work with a group.</h1>
+
+    <h2>Videos:</h2>
+    <ol>
+        <li><a targe="_blank" href="https://youtu.be/PZMU6iFHYa8">Group - git work flow group overview</a></li>
+        <li><a targe="_blank" href="https://youtu.be/X5y7cdxljxw">Group - add group members</a></li>
+        <li><a targe="_blank" href="https://youtu.be/QSRnuNh7gGA">Group - check out branch</a></li>
+        <li><a targe="_blank" href="https://youtu.be/IKzxl6JXohM">Group - merging branch</a></li>
+        <li><a targe="_blank" href="https://youtu.be/q7MB8KTq2P8">Group - merging branch from group member</a></li>
+    </ol>
+
+    <h2>Tasks</h2>
+    <form>
+
+
+        <p>Git Commands Working in a group:</p>
+<fieldset>
+    <legend>Project Manager</legend>
+        <label><input type="checkbox"> Set up a project on your desktop in cs142/dev-lab3.</label>
+        <label><input type="checkbox"> Create a new project in gitlab and save the git commands (Be sure the commands are for SSH). You will need to send the git add remote origin commands to your group members</label>
+
+        <label><input type="checkbox"> On your computer initialize git in your project folder<br>
+            <code class="plain">git init</code></label>
+        <label><input type="checkbox"> Connect to your repo<br>
+            <code class="plain">git remote add origin git@gitlab.uvm.edu:YOURNAME/YOUR-REPO-NAME.git</code> 
+            <br>ex: <br><code class="plain">git remote add origin git@gitlab.uvm.edu:robert-erickson/cs148b.git</code>
+        </label>
+        <label><input type="checkbox"> Add the files to commit<br>
+            <code class="plain">git add -A</code></label>
+        <label><input type="checkbox">Commit them locally<br>
+            <code class="plain">git commit -m "message about what you did"</code></label>
+        <label><input type="checkbox"> Push to the cloud<br>
+            <code class="plain">git push origin master</code></label>
+        <label><input type="checkbox"> Your project is all set up.</label>
+
+
+        <label><input type="checkbox"> On the git lab website add your group members to the project.</label>
+    </fieldset> 
+    
+        <fieldset>
+            <legend>Group members</legend>
+            <p>Set up your local machine</p>
+        <label><input type="checkbox"> Make a folder</label>
+        <label><input type="checkbox"> Initial the folder as a repo<br>
+            <code class="plain">git init</code></label>
+        <label><input type="checkbox"> Add the remote that was emailed by the project manager.<br>
+            <code class="plain">git remote add origin git@gitlab.uvm.edu:MANAGERUSERNAME/REPO-NAME.git</code>
+        </label>
+
+        <p>Group work flow. </p>
+        <p>Every Group member needs to follow these steps, <strong>every time without fail</strong> before working on any file. All commands are for your local computer.</p>
+        <label><input type="checkbox"> Make sure you have the groups master copy<br>
+            <code class="plain">git pull origin master</code></label>
+        <label><input type="checkbox"> Check out a branch to work on so you don't mess up the master. Name the branch with your initials (all caps) followed by a dash and a short name of the task you are working on. For example I am going to work on the navigation<br>
+            <code class="plain">git checkout -b RE_nav</code></label>
+        <label><input type="checkbox"> Make changes to your code as you would normally</label>
+        <label><input type="checkbox"> Sftp (run your NetBeans project) to server so you can make sure it works</label>
+        <label><input type="checkbox"> Verify your changes are working</label>
+        <label><input type="checkbox"> Add your changes to git (you can do git status first to make sure the files changed are the ones you expected to have changed).<br>
+            <code class="plain">git add -A</code></label>
+        <label><input type="checkbox"> Commit your changes locally<br>
+            <code class="plain">git commit -m "Added new menu item"</code></label>
+        <label><input type="checkbox"> Push your <strong>branch</strong> to gilab<br>
+            <code class="plain">git push origin RE_Nav</code></label>
+        <label><input type="checkbox"> On the gitlab website look at your branch and submit a merge request. A member of the team should review the code to make sure it meets the teams standards (ie code review). At this point teh project mananger should make sure that the new code will not cause the site code to break.</label>
+        <label><input type="checkbox"> Once the code is approved on the gitlab website (people can make comments) the manager would then Accept the Merge Request (check the delete the branch as it's not needed anymore)</label>
+        <label><input type="checkbox"> Person who created the branch should now delete the branch locally on their machine. Before deleting you need to switch back to the master and pull the changes down<br>
+
+            <code class="plain">git checkout master</code><br>
+
+            <code class="plain">git pull origin master</code><br>
+            <code class="plain">git branch -D RE_Nav</code>
+        </label>
+
+        </fieldset>
+        
+        <fieldset>
+            <legend>Work Flow Process</legend>
+        <p>To summerize: work flow process (this is what you do daily) working in a group:</p>
+
+        <label><input type="checkbox"> On your computers develop folder you <br>
+            <code class="plain">git pull origin master</code><br>
+            <code class="plain">git checkout -B INITIALS_task</code>
+        </label>
+        <label><input type="checkbox"> Create/edit your php pages</label>
+        <label><input type="checkbox"> sSftp (run your NetBeans project) your files to your develop folder</label>
+        <label><input type="checkbox"> Verify that the pages are working on the develop server</label>
+        
+        <p>Locally on your computer in the development folder<br>
+        <label><input type="checkbox"> <code class="plain">git status</code> </label>
+        <label><input type="checkbox"> <code class="plain">git add -A</code></label>
+        <label><input type="checkbox"> <code class="plain">git commit -m "message about what you did"</code></label>
+        <label><input type="checkbox"> <code class="plain">git push origin INITIALS_task</code></label>
+        <label><input type="checkbox"> Submit a merge request</label>
+        <label><input type="checkbox"> Request is reviewed</label>
+        <label><input type="checkbox"> Accept merge and delete branch</label>
+        <label><input type="checkbox"> <code class="plain">git checkout master</code></label>
+        <label><input type="checkbox"> <code class="plain">git pull origin master</code></label>
+        <label><input type="checkbox"> <code class="plain">git branch -D INITIALS_task</code></label>
+        <label><input type="checkbox"> Repeat above till project is done.</label>
+
+        <p>Manager would then pull changes. For this class,  each  member needs to have the site live so everyone needs to  pull your changes to the live site.</p>
+        <label><input type="checkbox"> ssh to your hosting account<br>
+            <code class="plain">ssh w3.uvm.edu -l yourusername</code></label>
+        <label><input type="checkbox"> cd to your live folder</label>
+        <label><input type="checkbox"> <code class="plain">git pull origin master</code></label>
+        <label><input type="checkbox"> Verify that the pages are working on the live server</label>
+        </fieldset>
+        
+        <fieldset>
+            <legend>Practice</legend>
+            <label><input type="checkbox"> Following the work flow above one member should make a small change to one file.</label>
+   <label><input type="checkbox"> Member should submit merge request.</label>
+            <label><input type="checkbox"> Everyone should look at branch, comment.</label>
+            <label><input type="checkbox"> Manager should approve and merge.</label>
+            <label><input type="checkbox"> Now two members make a change to the same file. This should cause a merge conflcit.</label>
+            <label><input type="checkbox"> Resolve the merge conflict.</label>
+            <label><input type="checkbox"> Make a pdf (gitcommits.pdf) of you commits and save in your live-lab3 folder.</label>
+        </fieldset>
+            </form>
+</article>
+</body>
+</html>
